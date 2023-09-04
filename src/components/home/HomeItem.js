d0061ae17e9c0097a6d08b545b30354c2df0792f
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-04 18:13:26 -0500

    Add the structure of every item in the home page

--- src/components/home/HomeItem.js
+++ src/components/home/HomeItem.js
@@ -1,12 +1,21 @@
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkDriver } from '../../redux/drivers/DriversSlice';
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
 
-const HomeItem = () => {
+const HomeItem = ({ driverId, constructorName, constructorImg, numDrivers }) => {
   const dispatch = useDispatch();
 
   return (
-    <li />
+    <div>
+      <div style={{ backgroundImage: {constructorImg} }}>
+        <FaRegArrowAltCircleRight style={{ color: '#fff' }} />
+      </div>
+      <div>
+        <h4>{constructorName}</h4>
+        <span>{numDrivers}</span>       
+      </div>
+    </div>
   );
 };
 
