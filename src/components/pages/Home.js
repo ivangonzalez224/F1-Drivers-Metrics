bf2f121ff47a1b0d9ad2e674a3fa7552158f5f2f
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-04 08:23:12 -0500

    Add routing to the whole project

---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import HomeLogic from '../home/HomeLogic';
+import '../../assets/styles/home.css';
+
+const Home = () => (
+  <div className="main_home">
+    <HomeLogic />
+  </div>
+);
+
+export default Home;
