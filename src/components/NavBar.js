8ff66c57476bf0160e853593ccdddaf785204709
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-09-06 19:13:53 -0500

    Test if navBar component renders correctly elements found by id

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { FaMicrophone, FaRegSun } from 'react-icons/fa';
 const NavBar = () => (
   <nav>
     <ul>
-      <li id="nav_left">
+      <li id="nav_left" data-testid="nav_home_list">
         <NavLink to="/">ᐸ</NavLink>
         <span>2023 - 2014</span>
       </li>
@@ -13,8 +13,8 @@ const NavBar = () => (
         <span>most drivers</span>
       </li>
       <li id="nav_mic">
-        <FaMicrophone style={{ color: '#fff', margin: '0 15% 0 0' }} />
-        <FaRegSun style={{ color: '#fff' }} />
+        <FaMicrophone style={{ color: '#fff', margin: '0 15% 0 0' }} data-testid="mic_icon" />
+        <FaRegSun style={{ color: '#fff' }} data-testid="settings_icon" />
       </li>
     </ul>
   </nav>
---
+++ src/test/NavBar.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+test('renders NavBar with the nav-left list that contains the home icon ᐸ', async () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>,
+  );
+
+  const navhomeList = await screen.findByTestId('nav_home_list');
+  expect(navhomeList).toBeInTheDocument();
+});
+
+test('renders NavBar with the mic icon', async () => {
+    render(
+      <Router>
+        <NavBar />
+      </Router>,
+    );
+  
+    const micIcon = await screen.findByTestId('mic_icon');
+    expect(micIcon).toBeInTheDocument();
+});
+
+test('renders NavBar with the settings icon', async () => {
+    render(
+      <Router>
+        <NavBar />
+      </Router>,
+    );
+  
+    const settingsIcon = await screen.findByTestId('settings_icon');
+    expect(settingsIcon).toBeInTheDocument();
+});
